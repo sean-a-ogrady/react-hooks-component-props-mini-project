3a8f27d63857723f51263389efb2dd0db37c2b9c
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-10-04 09:40:06 -0400

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About({image, about}) {
+    return (
+        <aside>
+            <img src={image ? image : "https://via.placeholder.com/215"} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    )
+}
